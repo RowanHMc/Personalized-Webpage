30bdafc9189960f8755ad489bdbbe497511ad110
Rowan <rowanhadegu@gmail.com>
2026-07-19 10:15:36 +0300

    add Event listener, event prevebtdefaullt(), declare variables and console to target specific events, avoid default actions, read,store convert and print data

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const ageInput = document.getElementById("age");
 function calculateMonths(age){
     return age * 12
 }
+userForm.addEventListener("submit,"function(event){
+    event.preventDefault();
+    
+    const userName= nameInput.value;
+    const userAge= Number(ageInput.value);
+
+    console.log("name", userName);
+    console.log("age", userAge)
+
+})
