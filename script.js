b8e3992fdb6159f62d199988e23367a1625b9700
Rowan <rowanhadegu@gmail.com>
2026-07-19 09:49:18 +0300

    add a function to calculate age in months to make it  resusable later in the code

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const userForm = document.getElementById("userForm");
 const nameInput = document.getElementById("name");
 const ageInput = document.getElementById("age");
 
+function calculateMonths(age){
+    return age * 12
+}
